0825217352fd9b968232c615685426b202e6229c
z3orc <z3orc@users.noreply.github.com>
2022-08-07 19:16:42 +0200

    Change listeners

--- src/commands/stop.ts
+++ src/commands/stop.ts
@@ -7,25 +7,25 @@ export async function stop() {
 
     const client = net.createConnection({ port: 25585 }, () => {
         client.write(`stop\n`);
+
+        client.on("error", () => {
+            spinner.succeed("Server stopped");
+            process.exit();
+        });
+
+        client.on("exit", () => {
+            spinner.succeed("Server stopped");
+            process.exit();
+        });
+
+        client.on("close", () => {
+            spinner.succeed("Server stopped");
+            process.exit();
+        });
     });
 
-    client.on("error", () => {
-        spinner.succeed("Server stopped");
-        process.exit();
-    });
-
-    client.on("exit", () => {
-        spinner.succeed("Server stopped");
-        process.exit();
-    });
-
-    client.on("close", () => {
-        spinner.succeed("Server stopped");
-        process.exit();
-    });
-
-    setTimeout(() => {
-        spinner.fail("Could not stop server");
-        process.exit();
-    }, 60000);
+    // setTimeout(() => {
+    //     spinner.fail("Could not stop server");
+    //     process.exit();
+    // }, 60000);
 }
